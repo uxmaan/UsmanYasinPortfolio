68efcee713c4cfc18427939c305c1e3bd90fa7c0
Usman Yasin <87988110+uxmaan@users.noreply.github.com>
2023-05-05 11:53:05 +0500

    changes

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'itsuxmaan@gmail.com';
+  $receiving_email_address = 'usman.yasin@aivacreative.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'itsuxman5@gmail.com',
-    'password' => 'MaNi@1435',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'smtp.gmail.com',
+  //   'username' => 'itsuxman5@gmail.com',
+  //   'password' => 'MaNi@1435',
+  //   'port' => '587'
+  // );
 
 
   $contact->add_message( $_POST['name'], 'From');
